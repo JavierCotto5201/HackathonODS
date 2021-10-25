5bd177a7ad65ac756bf71f845ea011949a9ee35d
Mar19845 <mar19845@uvg.edu.gt>
2021-10-24 21:17:40 -0600

    Update Clases.js

--- src/Components/Curso/Clases.js
+++ src/Components/Curso/Clases.js
@@ -23,6 +23,100 @@ const Clases = [
         ],
     },
     {
+        name:'Estructura de Datos',
+        departamento:'Programacion',
+        descripcion:'Utilizar estructuras de datos',
+        id:'graficasXcomputadora',
+        temporada:[
+            {
+                cap:"Capitulo 1: Framebuffer",
+                link:'https://youtu.be/DnGe4AbpQQg',
+                id:1
+            },
+            {
+                cap:"Capitulo 2: Mapas de Bits",
+                link:'https://youtu.be/gwax1vtxaTM',
+                id:2
+            },
+            {
+                cap:"Capitulo 3: Historia de los videojuegos",
+                link:'https://youtu.be/8XTPIY4BEL0',
+                id:3
+            },
+        ],
+    },
+    {
+        name:'Python',
+        departamento:'Programacion',
+        descripcion:'Utilizar y comprender el lenguaje de programacion de Python',
+        id:'graficasXcomputadora',
+        temporada:[
+            {
+                cap:"Capitulo 1: Framebuffer",
+                link:'https://youtu.be/DnGe4AbpQQg',
+                id:1
+            },
+            {
+                cap:"Capitulo 2: Mapas de Bits",
+                link:'https://youtu.be/gwax1vtxaTM',
+                id:2
+            },
+            {
+                cap:"Capitulo 3: Historia de los videojuegos",
+                link:'https://youtu.be/8XTPIY4BEL0',
+                id:3
+            },
+        ],
+    },
+    {
+        name:'Fisica 1',
+        departamento:'Fisica',
+        descripcion:'Es un curso en el que se abordan los conceptos fundamentales del electromagnetismo y la óptica, ofreciendo la oportunidad  de  que  los  estudiantes  adquieran  la  experiencia  concreta  mediante  la realización de experimentos y el modelaje de fenómenos.',
+        id:'fisica3',
+        temporada:[
+            {
+                cap:"Capitulo 1: Fuerza Fundamentales",
+                link:'https://youtu.be/PtUU7fnxRqo'
+            },
+            {
+                cap:"Capitulo 2: Carga Electrica",
+                link:'https://youtu.be/zu3wL2el5Lc'
+            },
+            {
+                cap:"Capitulo 3: Campo Electrico",
+                link:'https://youtu.be/0KDehyJouIk'
+            },
+            {
+                cap:"Capitulo 4: Electric Field Ring",
+                link:'https://youtu.be/g9iS3daBnA0'
+            },
+        ],
+    },
+    {
+        name:'Fisica 2',
+        departamento:'Fisica',
+        descripcion:'Es un curso en el que se abordan los conceptos fundamentales del electromagnetismo y la óptica, ofreciendo la oportunidad  de  que  los  estudiantes  adquieran  la  experiencia  concreta  mediante  la realización de experimentos y el modelaje de fenómenos.',
+        id:'fisica3',
+        temporada:[
+            {
+                cap:"Capitulo 1: Fuerza Fundamentales",
+                link:'https://youtu.be/PtUU7fnxRqo'
+            },
+            {
+                cap:"Capitulo 2: Carga Electrica",
+                link:'https://youtu.be/zu3wL2el5Lc'
+            },
+            {
+                cap:"Capitulo 3: Campo Electrico",
+                link:'https://youtu.be/0KDehyJouIk'
+            },
+            {
+                cap:"Capitulo 4: Electric Field Ring",
+                link:'https://youtu.be/g9iS3daBnA0'
+            },
+        ],
+    },
+    {
         name:'Fisica 3',
         departamento:'Fisica',
         descripcion:'Es un curso en el que se abordan los conceptos fundamentales del electromagnetismo y la óptica, ofreciendo la oportunidad  de  que  los  estudiantes  adquieran  la  experiencia  concreta  mediante  la realización de experimentos y el modelaje de fenómenos.',
@@ -48,6 +142,62 @@ const Clases = [
     },
     {
         name:'Matemática Discreta',
+        departamento:'Matemática',
+        descripcion:'Matemática discreta no es en realidad el nombre de una rama tradicional de las matemáticas como el cálculo o el álgebra lineal, sino que es una descripción de un conjunto de ramas de la matemática',
+        id:'matediscreta',
+        temporada:[
+            {
+                cap:"Capitulo 1: Proposiciones Matematicas",
+                link:'https://drive.google.com/file/d/13pwGjdfbj-ls6T9EK3Xk2nN843ALsXuV/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 2: Argumentos",
+                link:'https://drive.google.com/file/d/1GudV5FiPGp5mXGaD_-WI007cybndnIuA/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 3: Tecnicas de Demostración: Parte 1",
+                link:'https://drive.google.com/file/d/1Qomz0eO1cWeQyTrkZLf7AsSRTDK-m0BC/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 4: Tecnicas de Demostración: Parte 2",
+                link:'https://drive.google.com/file/d/1wJ__qMFH_dBpFMEeX4CostuZv6FM6-nj/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 5: Invariantes e Inducción Matematica",
+                link:'https://drive.google.com/file/d/1TVkro8MZCY9-oFPUUdFPytvS-YxmRZ_G/view?usp=sharing'
+            },
+        ],
+    },
+    {
+        name:'Calculo 1',
+        departamento:'Matemática',
+        descripcion:'Matemática discreta no es en realidad el nombre de una rama tradicional de las matemáticas como el cálculo o el álgebra lineal, sino que es una descripción de un conjunto de ramas de la matemática',
+        id:'matediscreta',
+        temporada:[
+            {
+                cap:"Capitulo 1: Proposiciones Matematicas",
+                link:'https://drive.google.com/file/d/13pwGjdfbj-ls6T9EK3Xk2nN843ALsXuV/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 2: Argumentos",
+                link:'https://drive.google.com/file/d/1GudV5FiPGp5mXGaD_-WI007cybndnIuA/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 3: Tecnicas de Demostración: Parte 1",
+                link:'https://drive.google.com/file/d/1Qomz0eO1cWeQyTrkZLf7AsSRTDK-m0BC/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 4: Tecnicas de Demostración: Parte 2",
+                link:'https://drive.google.com/file/d/1wJ__qMFH_dBpFMEeX4CostuZv6FM6-nj/view?usp=sharing'
+            },
+            {
+                cap:"Capitulo 5: Invariantes e Inducción Matematica",
+                link:'https://drive.google.com/file/d/1TVkro8MZCY9-oFPUUdFPytvS-YxmRZ_G/view?usp=sharing'
+            },
+        ],
+    },
+    {
+        name:'Pensamiento Cuantitativo',
         departamento:'Matemática',
         descripcion:'Matemática discreta no es en realidad el nombre de una rama tradicional de las matemáticas como el cálculo o el álgebra lineal, sino que es una descripción de un conjunto de ramas de la matemática',
         id:'matediscreta',
